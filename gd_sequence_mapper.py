28f591803f7534e96f29a91858297f610dedd2dd
stationarysalesman <stationarysalesman@users.noreply.github.com>
2015-09-18 17:47:33 -0500

    Update gd_sequence_mapper.py

--- gd_sequence_mapper.py
+++ gd_sequence_mapper.py
@@ -2,7 +2,22 @@ __author__ = 'tyler'
 """Genomediff Parser. Copyright Tyler Camp.
 
 This Python script takes genomediff files as input and outputs graphical information about
-the mutations identified in the files."""
+the mutations identified in the files.
+
+This file is part of gdparse.
+
+    gdparse is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    gdparse is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with gdparse.  If not, see <http://www.gnu.org/licenses/>."""
 
 from Bio import SeqIO
 import os
